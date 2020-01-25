dc98ca5e058338244a4c197efd36c204fba22339
Uzair Mohammed <uzairfaraz@gmail.com>
2020-01-24 23:45:53 -0600

    added hw0

---
+++ fizzbuzz.js
@@ -0,0 +1,18 @@
+function fizzBuzzOutput(value) {
+  if (value % 15 === 0) {
+    return "FizzBuzz";
+  } else if (value % 5 === 0) {
+    return "Buzz";
+} else if  (value % 3 === 0) {
+  return "Fizz";
+} else {
+  return value.toString();
+}
+}
+
+for (let i = 1; i < 101; i++) {
+  console.log(fizzBuzzOutput(i));
+}
+
+
+module.exports.fizzBuzzOutput = fizzBuzzOutput;
